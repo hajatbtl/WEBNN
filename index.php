17a9c3f6ae5f8d992bb970457f5bd9f7146537d7
unknown <you@example.com>
2023-08-29 17:41:26 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,301 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+  <!-- Style -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>
+    Notez-Nous
+  </title>
+
+  <link href="https://db.onlinewebfonts.com/c/261bd41f743f968436b6709532d4343e?family=PP+Pangram+Sans+Rounded+Medium"
+    rel="stylesheet">
+</head>
+
+<body>
+
+
+
+  <div
+    style=" line-height: 40px; height: 40px; background: linear-gradient(37deg, rgba(237,124,20,1) 36%, rgba(250,235,161,1) 95%); ">
+
+
+
+    <p class="  text-white text-center">Notez-Nous fait peau neuve ! Nous sommes fiers de vous présenter notre nouveau
+      site internet</p>
+
+  </div>
+
+
+  <?php include 'header.php' ?>
+
+  <div class="hero" style="background-image: url('images/bg.png');">
+
+    <div class="row">
+      <div class="position-relative">
+        <div class="image-with-overlay">
+          <img class="mt-5 pt-1" src="images/p0.png" alt="" style="max-width: 100%;">
+          <div class="overlay-text"
+            style="text-align: center; position: absolute; top: 90%; left: 50%; transform: translate(-50%, -50%);">
+            <h2 style="color: #000699;">Les 5 étoiles qui feront toute <br>la différence pour votre entreprise.</h2>
+            <p style="color: #000699;">Notez-Nous est LE service de notation qui s'occupe
+              d'améliorer <br>
+              votre réputation en ligne sans dégrader l'image de votre marque et déranger vos
+              clients.</p>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="mt-3 col-12 d-flex align-items-center justify-content-center">
+        <img src="images/p1.png" alt="" style="max-width: 6%;">
+        <p class="ml-2 mt-2" style="color:#4245a2;">Essayez comme +350 entreprises !</p>
+      </div>
+
+
+
+      <div class="col-12">
+        <div class="mt-5 button-container d-flex align-items-center justify-content-center">
+          <a class="button3" href="#contact">
+            Démarrer Maintenant
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+              class="ml-2 bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+            </svg>
+          </a>
+          <a class="bouton2 inscription-button ml-2 rounded-pill " href="#contact"><svg class="mr-2"
+              xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right"
+              viewBox="0 0 16 16">
+              <path
+                d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+            </svg>Voir nos dernières nouveautés </a>
+        </div>
+      </div>
+
+
+      <div class="col-12" style="background-repeat: no-repeat;background-image: url('images/p2.png');">
+        <div class="mt-5 video-container">
+          <iframe width="760" height="415" src="https://www.youtube.com/embed/o_RFSjF8XKM?si=1HRyPiUFyLLQ7Hua"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen></iframe>
+        </div>
+      </div>
+
+
+      <div class="col-12 "
+        style="background-size: contain; background-position:right; background-repeat: no-repeat;background-image: url('images/p3.png');">
+        <div style="border-radius:40px;" class="shadow-lg p-3 mb-5 bg-body  col-8  m-auto bg-light">
+          <div style="justify-content: space-around;" class="d-flex p-5">
+
+            <div class="col-6">
+
+              <h1 style="color: #000699;"><strong>Nous rendons visible
+                  la satisfaction de vos clients à votre profit.</strong></h1>
+
+
+              <div class="col-12 mt-5 d-flex "
+                style="flex-wrap: wrap ; gap:10px; width: max-content !important;height: max-content;">
+
+                <a class="button5 p-1    " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> Artisant
+
+                </a>
+                <a class="button5 p-1    " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> BTP
+
+                </a>
+                <a class="button5 p-1  " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> Infirmier
+
+                </a>
+                <a class="button5 p-1    " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> Medecin
+
+                </a>
+                <a class="button5 p-1   " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> Gestionnaires
+
+                </a>
+                <a class="button5 p-1   " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> Artisant
+
+                </a>
+                <a class="button5 p-1    " href="#contact">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-1 bi bi-hand-index-thumb" viewBox="0 0 16 16">
+                    <path
+                      d="M6.75 1a.75.75 0 0 1 .75.75V8a.5.5 0 0 0 1 0V5.467l.086-.004c.317-.012.637-.008.816.027.134.027.294.096.448.182.077.042.15.147.15.314V8a.5.5 0 0 0 1 0V6.435l.106-.01c.316-.024.584-.01.708.04.118.046.3.207.486.43.081.096.15.19.2.259V8.5a.5.5 0 1 0 1 0v-1h.342a1 1 0 0 1 .995 1.1l-.271 2.715a2.5 2.5 0 0 1-.317.991l-1.395 2.442a.5.5 0 0 1-.434.252H6.118a.5.5 0 0 1-.447-.276l-1.232-2.465-2.512-4.185a.517.517 0 0 1 .809-.631l2.41 2.41A.5.5 0 0 0 6 9.5V1.75A.75.75 0 0 1 6.75 1zM8.5 4.466V1.75a1.75 1.75 0 1 0-3.5 0v6.543L3.443 6.736A1.517 1.517 0 0 0 1.07 8.588l2.491 4.153 1.215 2.43A1.5 1.5 0 0 0 6.118 16h6.302a1.5 1.5 0 0 0 1.302-.756l1.395-2.441a3.5 3.5 0 0 0 .444-1.389l.271-2.715a2 2 0 0 0-1.99-2.199h-.581a5.114 5.114 0 0 0-.195-.248c-.191-.229-.51-.568-.88-.716-.364-.146-.846-.132-1.158-.108l-.132.012a1.26 1.26 0 0 0-.56-.642 2.632 2.632 0 0 0-.738-.288c-.31-.062-.739-.058-1.05-.046l-.048.002zm2.094 2.025z" />
+                  </svg> BTP
+
+                </a>
+
+
+
+
+              </div>
+              <div class="col-12 mt-5">
+                <a class="button4 " href="#contact">
+                  Démarrer Maintenant
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-2 bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+
+            <div class="col-4">
+              <img class="rounded-7" src="images/p4.png" alt="" style="max-width: 100%; transform: rotate(12deg);">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div class="col-12 mt-3"
+      style="background-repeat: no-repeat;background-image: url('images/p2.png');">
+        <div style="border-radius:40px;" class="shadow-lg p-3 mb-5 bg-body  col-8  m-auto bg-light">
+          <div style="justify-content: space-around;" class="d-flex p-5">
+            <div class="col-4">
+              <img class="rounded-lg" src="images/p6.png" alt="" style="max-width: 120%;">
+            </div>
+            <div class="col-6">
+
+              <h1 class="text-justify" style="color: #000699;"><strong>Nous rendons visible
+                  la satisfaction de vos clients à votre profit.</strong></h1>
+              <p class="text-justify" style="color: #000699;">Notez-Nous est LE service de notation qui s'occupe
+                d'améliorer <br>
+                votre réputation en ligne sans dégrader l'image de votre marque et déranger vos
+                clients.</p>
+
+
+              <div class="col-12 mt-5">
+                <a class="button6 " href="#contact">
+                  Démarrer Maintenant
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-2 bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+
+
+
+          </div>
+        </div>
+      </div>
+
+
+      <div class="col-12 mt-3"
+        style="background-size: contain; background-position:right;  background-repeat: no-repeat;background-image: url('images/p3.png');">
+        <div style="border-radius:40px;" class="shadow-lg p-3 mb-5 bg-body  col-8  m-auto bg-light">
+          <div style="justify-content: space-around;" class="d-flex p-5">
+
+            <div class="col-6">
+<p style="font-family: cursive; font-size: smaller;color: #d89653;"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class=" mr-2 bi bi-gem" viewBox="0 0 16 16">
+  <path d="M3.1.7a.5.5 0 0 1 .4-.2h9a.5.5 0 0 1 .4.2l2.976 3.974c.149.185.156.45.01.644L8.4 15.3a.5.5 0 0 1-.8 0L.1 5.3a.5.5 0 0 1 0-.6l3-4zm11.386 3.785-1.806-2.41-.776 2.413 2.582-.003zm-3.633.004.961-2.989H4.186l.963 2.995 5.704-.006zM5.47 5.495 8 13.366l2.532-7.876-5.062.005zm-1.371-.999-.78-2.422-1.818 2.425 2.598-.003zM1.499 5.5l5.113 6.817-2.192-6.82L1.5 5.5zm7.889 6.817 5.123-6.83-2.928.002-2.195 6.828z"/>
+</svg>Pourquoi Notez-Nous ?</p>
+              <h1 class="text-justify" style="color: #000699;"><strong>Nous rendons visible
+                  la satisfaction de vos clients à votre profit.</strong></h1>
+              <p class="text-justify" style="color: #000699;">La gestion de votre réputation peut être complexe lorsque vous n'avez pas le contrôle sur les avis. 
+              Vos clients publient ce qu'ils veulent sur vous, et quand ils veulent. Dans un monde où le client est roi, la maitrise de la satisfaction
+               client devient chronophrage et parfois embêtante pour un chef d'entreprise.<br>C'est pourquoi Notez-Nous regroupe toutes les fonctionnalités
+                essentielles dont vous avez besoin, telles que la collecte des avis, la gestion des formulaires et des statistiques, en une seule plateforme
+                 intuitive. 
+                 <br><strong>Avec Notez-Nous, unifiez vos processus, gagnez en productivité et impressionnez vos clients. Libérez-vous des tâches et 
+              concentrez-vous sur le développement de votre entreprise. Simplifiez votre flux de travail et maitrisez enfin votre image.</strong></p>
+
+
+              <div class="col-12 mt-5">
+                <a class="button6 " href="#contact">
+                Essayez comme Sarah, médecin depuis 10 ans
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="ml-2 bi bi-arrow-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                      d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            <div class="col-4">
+              
+              <img class="rounded-lg mt-4" src="images/p7.png" alt="" style=" max-width: 150%;">
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+
+
+
+  </div>
+
+
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
